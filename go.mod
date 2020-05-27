51829393eaf38bd4f50c9746e5b03304bc4268ce
Chin Dou <cdou.swsec@gmail.com>
2020-05-26 20:25:48 -0400

    fix(mod/gitignore): update go mod resources after build and add cmd package

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/walmartdigital/go-kaya
 
 go 1.14
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20200428143746-21a406dcc535
+	github.com/go-resty/resty/v2 v2.3.0
+	github.com/golang/mock v1.4.3
+	github.com/spf13/pflag v1.0.5
+	go.uber.org/zap v1.15.0
+	sigs.k8s.io/controller-runtime v0.6.0
+)
---
+++ pkg/cmd/cli.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"time"
+
+	flag "github.com/spf13/pflag"
+	"github.com/walmartdigital/go-kaya/pkg/client"
+	"github.com/walmartdigital/go-kaya/pkg/kafkaconnect"
+	"go.uber.org/zap"
+)
+
+func readConfig(file string) *kafkaconnect.Connector {
+	var config kafkaconnect.Connector
+
+	content, err0 := ioutil.ReadFile(file)
+	if err0 != nil {
+		zap.L().Error(err0.Error())
+		return nil
+	}
+
+	err1 := json.Unmarshal(content, &config)
+
+	if err1 != nil {
+		zap.L().Error(err1.Error())
+		return nil
+	}
+
+	return &config
+}
+
+func main() {
+	var host string
+	var configFile string
+	var action string
+	var connector string
+
+	var logger *zap.Logger
+	var err error
+	logLevel := os.Getenv("LOG_LEVEL")
+
+	if logLevel == "DEBUG" {
+		logger, err = zap.NewDevelopment()
+	} else {
+		logger, err = zap.NewProduction()
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	zap.ReplaceGlobals(logger)
+	zap.L().Debug("Logger initialized, writing to stdout")
+
+	flag.StringVarP(&host, "addr", "a", "", "Kafka Connect address in the form of <host:port>")
+	flag.StringVarP(&configFile, "file", "f", "", "Path to connector config file")
+	flag.StringVarP(&action, "cmd", "c", "", "Action to perform against Kafka Connect instance")
+	flag.StringVarP(&connector, "name", "n", "", "Connector name on which to perform action")
+
+	flag.Parse()
+
+	zap.L().Debug("Parsed command line flag 'host': " + host)
+	zap.L().Debug("Parsed command line flag 'file': " + configFile)
+	zap.L().Debug("Parsed command line flag 'action': " + action)
+	zap.L().Debug("Parsed command line flag 'connector':" + connector)
+
+	config := client.HTTPClientConfig{
+		Headers:            map[string]string{"Content-type": "application/json"},
+		AuthType:           client.NoneAuth,
+		RetryCount:         3,
+		RetryWaitTime:      1 * time.Second,
+		RetryWaitMaxTime:   30 * time.Second,
+		RetryConditionFunc: nil,
+	}
+
+	client, _ := kafkaconnect.NewClient(host, config, client.RestyClientFactory{})
+
+	switch action {
+	case "read":
+		response, err := client.Read(connector)
+		if err != nil {
+			zap.L().Error(err.Error())
+		} else {
+			bytes, _ := json.Marshal(response)
+			zap.L().Info(string(bytes))
+		}
+	case "status":
+		response, err := client.Status(connector)
+		if err != nil {
+			zap.L().Error(err.Error())
+		} else {
+			bytes, _ := json.Marshal(response)
+			zap.L().Info(string(bytes))
+		}
+	case "create":
+		if configFile == "" {
+			zap.L().Error("If action is 'create', a configuration file is required")
+			return
+		}
+		config := readConfig(configFile)
+		if config != nil {
+			response, err := client.Create(*config)
+			if err != nil {
+				zap.L().Error(err.Error())
+				return
+			}
+			if response.Result == "success" {
+				zap.L().Info("Connector created successfully")
+
+				bytes, err1 := json.Marshal(response.Payload)
+				if err1 != nil {
+					zap.L().Error(err1.Error())
+					return
+				}
+				zap.L().Info(string(bytes))
+			}
+		}
+	case "update":
+		if configFile == "" {
+			zap.L().Error("If action is 'create', a configuration file is required")
+			return
+		}
+		config := readConfig(configFile)
+		if config != nil {
+			response, err := client.Update(*config)
+			if err != nil {
+				zap.L().Error(err.Error())
+				return
+			}
+			if response.Result == "success" {
+				zap.L().Info("Connector updated successfully")
+
+				bytes, err1 := json.Marshal(response.Payload)
+				if err1 != nil {
+					zap.L().Error(err1.Error())
+					return
+				}
+				zap.L().Info(string(bytes))
+			}
+		}
+	case "delete":
+		response, err := client.Delete(connector)
+		if err != nil {
+			zap.L().Error(err.Error())
+		}
+		if response.Result == "success" {
+			zap.L().Info("Connector deleted successfully")
+		} else {
+			zap.L().Info("Could not delete connector")
+		}
+	default:
+		zap.L().Fatal("Invalid action requested")
+	}
+}
